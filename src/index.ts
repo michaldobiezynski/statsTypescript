31b245998a43c415b4986fa0ed26e3db8c7a7370
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-07-19 20:35:02 +0100

    Updating Reader References

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchResult } from "./MatchResult";
 import { MatchReader } from "./MatchReader";
 
-const reader = new MatchReader("football.csv");
-reader.read();
+const csvFileReader = new CsvFileReader("football.csv");
 
-// console.log(reader.data);
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-// console.log(matches);
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {

d4f737f0ee7b4844325d0055020548c0993bef29
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-07-19 20:22:33 +0100

    Alternate Refactor

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { MatchReader } from "./MatchReader";
 const reader = new MatchReader("football.csv");
 reader.read();
 
-console.log(reader.data);
+// console.log(reader.data);
 
 // console.log(matches);
 let manUnitedWins = 0;

4167e7a23957b0b3dca937cd00bf0e8b609496be
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-07-19 20:30:12 +0100

    Transforming Data

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -2,24 +2,28 @@ import { dateStringToDate } from "./utils";
 import { MatchResult } from "./MatchResult";
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-
 interface DataReader {
   read(): void;
   data: string[][];
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
+
   constructor(public reader: DataReader) {}
 
-//   .map((row: string[]): MatchData => {
-//     return [
-//       dateStringToDate(row[0]),
-//       row[1],
-//       row[2],
-//       parseInt(row[3]),
-//       parseInt(row[4]),
-//       row[5] as MatchResult,
-//       row[6],
-//     ];
-//   });
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult,
+        row[6],
+      ];
+    });
+  }
 }
